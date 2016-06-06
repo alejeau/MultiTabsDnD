8de8fa0002eb9699c38f993ae1b3c0e04946c7da
alejeau@excilys <alejeau.excilys@desktop-0134.ebi.excilys.com>
2016-06-06 14:33:27 +0200

    Real mutli tab DnD POC --functionnal.

--- app/scripts/dnd.v2.js
+++ app/scripts/dnd.v2.js
@@ -6,9 +6,10 @@
 // All the 'e.target' are normally interchangeable with 'this'.
 'use strict';
 //*
-var dragSrcEl = null;
+// var dragSrcEl = null;
 var ownControllerUUID = {uuid: getGuid()};
 var otherControllerUUID = {uuid: null};
+var linearColumns;
 var exchangeFile = null;
 
 // will be stored w/ a localStorage using ownControllerUUID as key
@@ -32,17 +33,17 @@ var cols = [document.querySelectorAll('#columns1 .column'),
 /* ********************************** */
 
 // Perso
-var initF = init;
-var findObjectViaInnerHTMLF = findObjectViaInnerHTML;
-var getGuidF = getGuid;
+// var init = init;
+// var findObjectViaInnerHTML = findObjectViaInnerHTML;
+// var getGuid = getGuid;
 
 // Drag'n'Drop (DnD)
-var handleDragStartF = handleDragStart;
-var handleDragEnterF = handleDragEnter;
-var handleDragOverF = handleDragOver;
-var handleDragLeaveF = handleDragLeave;
-var handleDropF = handleDrop;
-var handleDragEndF = handleDragEnd;
+// var handleDragStart = handleDragStart;
+// var handleDragEnter = handleDragEnter;
+// var handleDragOver = handleDragOver;
+// var handleDragLeave = handleDragLeave;
+// var handleDrop = handleDrop;
+// var handleDragEnd = handleDragEnd;
 
 
 // Créer des objets représentants les objets dispo avec ou non un UUID
@@ -56,16 +57,15 @@ function handleDragStart(e) {
     }
     // Target (this) element is the source node.
     e.target.style.opacity = '0.4';
+    e.dataTransfer.effectAllowed = 'move';
 
-    dragSrcEl = e.target;
+    // dragSrcEl = e.target;
 
     var json = findObjectViaInnerHTML(e.target.innerHTML);
     console.log(json);
 
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', e.target.innerHTML);
-    localStorage.setItem('fileStorage', JSON.stringify(json));
-    // console.log(e.target);
+    // e.dataTransfer.setData('text/html', e.target.innerHTML);
+    localStorage.setItem('exchangeFile', JSON.stringify(json));
 }
 
 function handleDragOver(e) {
@@ -73,7 +73,6 @@ function handleDragOver(e) {
         // Prevents the default behavior of a browser; for instance preventing link opening to allow the DnD.
         e.preventDefault();
     }
-
     e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
 
     // Some browsers may not need this, but some do so we add it
@@ -95,17 +94,24 @@ function handleDragLeave(e) {
     e.target.classList.remove('over');  // this / e.target is previous target element.
 }
 
-function handleDrop(e) {
-    // this/e.target is current target element.
-
+function handleDrop(e) { // this/e.target is current target element.
     if (e.stopPropagation) {
         e.stopPropagation(); // Stops some browsers from redirecting.
     }
 
+    // We get the object that's being dragged/dropped
+    var source = JSON.parse(localStorage.getItem('exchangeFile'));
+
     // Don't do anything if dropping the same column we're dragging.
-    if (dragSrcEl != e.target) {
-        var json = JSON.parse(localStorage.getItem('fileStorage'));
-        console.log(json);
+    if (e.target.innerHTML !== source.body) {
+        // console.log(source);
+        var target = findObjectViaInnerHTML(e.target.innerHTML);
+        var tmp = target.body;
+        e.target.innerHTML = source.body;
+        target.body = source.body;
+        updateFileStorage(target);
+        source.body = tmp;
+        localStorage.setItem('exchangeFile', JSON.stringify(source));
     }
 
 
@@ -114,28 +120,45 @@ function handleDrop(e) {
 
 function handleDragEnd(e) {
     // this/e.target is the source node.
+    var json = JSON.parse(localStorage.getItem('exchangeFile'));
+    linearColumns = JSON.parse(localStorage.getItem('linearColumns' + ownControllerUUID.uuid));
 
-    [].forEach.call(cols, function (c) {
-        c.forEach.call(c, function (col) {
-            col.classList.remove('over');
-        });
-    });
+    console.log(json);
+
+    var i, j, k = 0;
+    for (i = 0; i < cols.length; i++) {
+        for (j = 0; j < cols[i].length; j++) {
+            cols[i][j].classList.remove('over');
+            if (linearColumns[k].uuid === json.uuid) {
+                e.target.innerHTML = json.body;
+                e.target.style.opacity = '1';
+            }
+            k++;
+        }
+    }
+    updateFileStorage(json);
+    // var json = JSON.parse(localStorage.getItem('fileStorage' + ownControllerUUID.uuid));
 }
 
+function addEventListeners() {
+    console.log(cols);
 
-[].forEach.call(cols, function (c) {
-    c.forEach.call(c, function (col) {
-        col.addEventListener('dragstart', handleDragStart, false);
-        col.addEventListener('dragenter', handleDragEnter, false);
-        col.addEventListener('dragover', handleDragOver, false);
-        col.addEventListener('dragleave', handleDragLeave, false);
-        col.addEventListener('drop', handleDrop, false);
-        col.addEventListener('dragend', handleDragEnd, false);
-    });
-});
+    var i, j;
+    for (i = 0; i < cols.length; i++) {
+        for (j = 0; j < cols[i].length; j++) {
+            cols[i][j].addEventListener('dragstart', handleDragStart, false);
+            cols[i][j].addEventListener('dragenter', handleDragEnter, false);
+            cols[i][j].addEventListener('dragover', handleDragOver, false);
+            cols[i][j].addEventListener('dragleave', handleDragLeave, false);
+            cols[i][j].addEventListener('drop', handleDrop, false);
+            cols[i][j].addEventListener('dragend', handleDragEnd, false);
+        }
+    }
+}
 
 // Gets the original objects
 function init() {
+    addEventListeners();
 
     console.log(localStorage.length);
     // We put to everybody's sight our UUID
@@ -143,8 +166,10 @@ function init() {
     var uuidList = [];
     var empty = JSON.stringify({uuidList: []});
 
+    console.log(tmp);
+
     // if empty, we populate
-    if (tmp === empty) {
+    if ((tmp === empty) || (tmp === null)) {
         uuidList.push(ownControllerUUID);
         uuidStorage = {uuidList: uuidList};
     } else { // if not, we complement
@@ -152,18 +177,27 @@ function init() {
         otherControllerUUID = uuidStorage.uuidList[0].uuid;
         uuidStorage.uuidList.push(ownControllerUUID);
     }
+
     var string = JSON.stringify(uuidStorage);
     localStorage.setItem('uuidStorage', string);
 
     fileStorage = [];
+    linearColumns = [];
 
-    var i, j;
+    // populating fileStorage
+    var i, j, k = 0;
     for (i = 0; i < cols.length; i++) {
         for (j = 0; j < cols[i].length; j++) {
             tmp = {uuid: getGuid(), body: cols[i][j].innerHTML};
             fileStorage.push(tmp);
+            linearColumns.push({colNum: k, uuid:tmp.uuid});
+            k++;
         }
     }
+    // Storing fileStorage in the local storage
+    localStorage.setItem('fileStorage' + ownControllerUUID.uuid, JSON.stringify(fileStorage));
+    localStorage.setItem('linearColumns' + ownControllerUUID.uuid, JSON.stringify(linearColumns));
+
 }
 
 function clearAll() {
@@ -173,10 +207,15 @@ function clearAll() {
 
 
 function list() {
-    for(var i=0, len=localStorage.length; i<len; i++) {
-        var key = localStorage.key(i);
-        var value = localStorage[key];
-        console.log(key + ' => ' + value);
+    var len = localStorage.length;
+    if (len === 0) {
+        console.log('The localStorage is empty!');
+    } else {
+        for (var i = 0; i < len; i++) {
+            var key = localStorage.key(i);
+            var value = localStorage[key];
+            console.log(key + ' => ' + value);
+        }
     }
 }
 
@@ -198,11 +237,31 @@ function store(id, jsonObject) {
     localStorage.setItem(id, value);
 }
 
+function updateFileStorage(json) {
+    fileStorage = JSON.parse(localStorage.getItem('fileStorage' + ownControllerUUID.uuid));
+
+    // populating fileStorage
+    var i;
+    for (i = 0; i < cols.length; i++) {
+        if (fileStorage[i].uuid === json.uuid) {
+            fileStorage[i].body = json.body;
+            break;
+        }
+    }
+
+    // Storing fileStorage in the local storage
+    localStorage.setItem('fileStorage' + ownControllerUUID.uuid, JSON.stringify(fileStorage));
+}
+
 function getGuid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
+}
+
+function getOwn(field) {
+    return field + ownControllerUUID;
 }
 
 function nodeToString(node) {
